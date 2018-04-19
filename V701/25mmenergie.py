d94a4ca861809061ce8a38342b52b3bede85152d
Tom Rosenstein <tom.rosenstein@tu-dortmund.de>
2018-04-19 13:51:49 +0200

    hab mal alles mit make fertig gemacht, bei mir  kommen die plots nicht raus.  Versuch mal ob es bei dir klappt.

--- V701/25mmenergie.py
+++ V701/25mmenergie.py
@@ -10,10 +10,9 @@ energie = kanal / 1199 * 4
 
 plt.plot(druck,energie)
 
-#plt.xlabel(r'Counts pro $10 \si{\seconds}$')
-#plt.ylabel(r'Absolute Häufigkeit')
-#plt.legend(loc='best')
+plt.xlabel(r'Druck/ $\si{\milli\bar}$')
+plt.ylabel(r'Energie/ $\mega\electronvolt$')
+plt.legend(loc='best')
 # in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-#plt.savefig('build/histo.pdf')
-plt.show()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/25mmenergie.pdf')
