4b8c17bdb9028a984b8db9130605628453eb0f7c
Tom Rosenstein <tom.rosenstein@tu-dortmund.de>
2018-04-26 18:52:52 +0200

    V406 erstellt. Daten eingegeben. Plots angefangen.

---
+++ V406/ds1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+x,I = np.genfromtxt("data/ds1.csv",delimiter = ",", unpack=True)
+
+"""
+def f(x, a, b):
+    return a*x +b
+
+params, covariance_matrix = curve_fit(f, xeff, energie)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print('a=', params[0], '+-', errors[0])
+print('b=', params[1], '+-', errors[1])
+
+"""
+plt.plot(x, I, 'k.', label="Daten", ms=2.5)
+#plt.plot(xeff, f(xeff, *params), 'r-', label='Fit')
+
+
+plt.xlabel(r'Position / mm ')
+plt.ylabel(r'Photostrom')
+plt.legend(loc='best')
+plt.grid()
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/ds1.pdf')
