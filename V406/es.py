8507189ea9ce95b9b9283e69f686598e306c023f
Fabian Koch <fabian3.koch@tu-dortmund.de>
2018-04-28 14:47:14 +0200

    Versucht mit Auswertung anzufangen
    Habe zumindest den die festen Werte aufgeschrieben
    Versucht den Fit für den Einzelspalt hinzubekommen, der will aber
    nicht, habe jetzt das x-array in zwei teile geteilt, so dass
    wir jetzt von -15 bis 15 gehen, dadurch ähnelt der fit wenigstens
    entfernt der Verteilung. Die andere Definiton klappt noch nicht,
    da dort bei 'phi'=0 durch 0 geteilt wird und ich selbst mit
    np.delete die entsprechenden arraystellen nicht herausbekomme.

--- V406/es.py
+++ V406/es.py
@@ -3,21 +3,26 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 x,I = np.genfromtxt("data/es.csv",delimiter = ",", unpack=True)
+x = x-24
+xbla= np.linspace(-15,15, 1000)
+lada = 635 * 10**(-6)
+L = 99.5 * 10
 
-"""
-def f(x, a, b):
-    return a*x +b
+def f(x, a, b, c):
+    #return a**2 * b**2 * (lada /( np.pi * b * np.sin(x/(L) + c )))**2 * np.sin(np.pi * b * np.sin(x /(L) + c ) / (lada))
+    return a ** 2 * b**2 * np.sinc(np.pi * b * np.sin(x/L+c)/(lada))**2
 
-params, covariance_matrix = curve_fit(f, xeff, energie)
+params, covariance_matrix = curve_fit(f, x, I, p0=(1.4, 0.075, 0))
 
 errors = np.sqrt(np.diag(covariance_matrix))
 
 print('a=', params[0], '+-', errors[0])
 print('b=', params[1], '+-', errors[1])
+print('c=', params[2], '+-', errors[2])
 
-"""
+
 plt.plot(x, I, 'k.', label="Daten", ms=2.5)
-#plt.plot(xeff, f(xeff, *params), 'r-', label='Fit')
+plt.plot(xbla, f(xbla, *params), 'r-', label='Fit')
 
 
 plt.xlabel(r'Position / mm ')
