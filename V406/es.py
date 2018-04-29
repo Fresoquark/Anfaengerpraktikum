aadc37afda23f136f4ac195d0f1ec93f78b6fd75
Fabian Koch <fabian3.koch@tu-dortmund.de>
2018-04-29 14:14:07 +0200

    Einzelspalt fertig

--- V406/es.py
+++ V406/es.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 x,I = np.genfromtxt("data/es.csv",delimiter = ",", unpack=True)
-x = x-24
-xbla= np.linspace(-15,15, 1000)
-lada = 635 * 10**(-6)
-L = 99.5 * 10
+x = x-24 #Zentrieren der Werte
+I = I-0.021 #Subtraktion des Dunkelstromes
+xbla= np.linspace(-15,15, 1000) #Weicherer Fit
+lada = 635 * 10**(-6) #Wellenlänge Lambda in Millimeter
+L = 99.5 * 10 #Länge L in Millimeter
 
 def f(x, a, b, c, d):
     #return a**2 * b**2 * (lada /( np.pi * b * np.sin(x/(L) + c )))**2 * np.sin(np.pi * b * np.sin(x /(L) + c ) / (lada))
-    return a ** 2 * b**2 * np.sinc(np.pi * b * np.sin(x/L+c)/(lada))**2 +d
+    return a**2 * b**2 * np.sinc(np.pi * b * np.sin(x/L+c)/(lada))**2 + d
 
 params, covariance_matrix = curve_fit(f, x, I, p0=(1.4, 0.075, 0, 0.3))
 
@@ -22,11 +23,11 @@ print('c=', params[2], '+-', errors[2])
 print('d=', params[3], '+-', errors[3])
 
 plt.plot(x, I, 'k.', label="Daten", ms=2.5)
-plt.plot(xbla, f(xbla, *params), 'r-', label='Fit')
+plt.plot(xbla, f(xbla, *params), 'r--', label='Fit')
 
 
 plt.xlabel(r'Position / mm ')
-plt.ylabel(r'Photostrom')
+plt.ylabel(r'Photostrom / µA')
 plt.legend(loc='best')
 plt.grid()
 # in matplotlibrc leider (noch) nicht möglich
