3b51e64ebef9c7d92511bac377ced14dec41394c
Tom Rosenstein <tom.rosenstein@tu-dortmund.de>
2018-04-30 12:40:22 +0200

    Captions bei Abbildungen mit "." beendet.  Gleichung mit \eqref für Klammern referenziert.  Fits für Doppelspalte erstellt bzw. verbessert.  Auswertung fertig.

--- V406/ds1.py
+++ V406/ds1.py
@@ -4,18 +4,17 @@ from scipy.optimize import curve_fit
 
 x,I = np.genfromtxt("data/ds1.csv",delimiter = ",", unpack=True)
 
-x = x-24.3 #Zentrieren der Werte
+x = x-23.6 #Zentrieren der Werte
 I = I-0.021 #Subtraktion des Dunkelstromes
 xbla= np.linspace(-15,15, 1000) #Weicherer Fit
 lada = 635 * 10**(-6) #Wellenlänge Lambda in Millimeter
 L = 99.5 * 10 #Länge L in Millimeter
-s= 0.25 #SPlatabstand in mm
 
-def f(x, a, b, c, d):
+def f(x, a, b, c, d, e):
     #return a**2 * b**2 * (lada /( np.pi * b * np.sin(x/(L) + c )))**2 * np.sin(np.pi * b * np.sin(x /(L) + c ) / (lada))
-    return  a**2 * np.sinc(np.pi * b * np.sin(x/L+c)/(lada))**2 * 4 * np.cos( (s * np.pi * np.sin(x/L+c)) / (lada) )**2 + d
+    return  a**2 * np.sinc(np.pi * b * np.sin(x/L+c)/(lada))**2 * 4 * np.cos( (e * np.pi * np.sin(x/L+c)) / (lada) )**2 + d
 
-params, covariance_matrix = curve_fit(f, x, I, p0=(1.4, 0.075, 0, 0.3))
+params, covariance_matrix = curve_fit(f, x, I, p0=(2.2, 0.15, 0, 2, 0.25))
 
 errors = np.sqrt(np.diag(covariance_matrix))
 
@@ -23,14 +22,14 @@ print('a=', params[0], '+-', errors[0])
 print('b=', params[1], '+-', errors[1])
 print('c=', params[2], '+-', errors[2])
 print('d=', params[3], '+-', errors[3])
+print('e=', params[4], '+-', errors[4])
 
 
 plt.plot(x, I, 'k.', label="Daten", ms=2.5)
 plt.plot(xbla, f(xbla, *params), 'r--', label='Fit')
 
-
-plt.xlabel(r'Position / mm ')
-plt.ylabel(r'Photostrom')
+plt.xlabel(r'Position /$\si{\milli\metre}$ ')
+plt.ylabel(r'Photostrom/$\si{\micro\ampere}$')
 plt.legend(loc='best')
 plt.grid()
 # in matplotlibrc leider (noch) nicht möglich
