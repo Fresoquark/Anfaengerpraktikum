343d61061dff0add366e5a4d86ff6f991e6b8a96
Fabian Koch <fabian3.koch@tu-dortmund.de>
2018-06-15 16:57:43 +0200

    Plots für blei und zink

--- V704/zink.py
+++ V704/zink.py
@@ -4,19 +4,17 @@ from scipy.stats import sem
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-t, cts, d = np.genfromtxt("data/blei.csv", delimiter=",", unpack=True)
+t, cts, d = np.genfromtxt("data/zink.csv", delimiter=",", unpack=True)
 cts = cts/t
 d=d*1e-4
 null=950/900
 cts=cts-null
 cts=np.log(cts)
-ctsfit=np.delete(cts,[0]) #Der letzte wird weggelassen, da dieser wert dank der hintergrundstrahlung negativ ist
-dfit=np.delete(d,[0])
 
 def f(x, a, b):
     return a*x+b
 
-params, covariance_matrix = curve_fit(f,dfit,ctsfit)
+params, covariance_matrix = curve_fit(f,d,cts)
 errors = np.sqrt(np.diag(covariance_matrix))
 
 print('a=', params[0], '+-', errors[0])
@@ -24,11 +22,11 @@ print('b=', params[1], '+-', errors[1])
 a=ufloat(params[0],errors[0])
 b=ufloat(params[1],errors[1])
 
-plt.plot(dfit, f(dfit, *params), 'r-', label='Linearer Fit')
+plt.plot(d, f(d, *params), 'r-', label='Linearer Fit')
 plt.plot(d, cts, 'k.', label="Datenpunkte", ms=2.5)
 plt.legend(loc="best")
 plt.grid()
 plt.xlabel(r"D / $\si{\metre}$")
 plt.ylabel(r'ln(Counts - Hintergrundstrahlungscounts) / $\si{\per\second}$ ')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/blei.pdf')
+plt.savefig('build/zink.pdf')
