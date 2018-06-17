87cadb03876fecf0e6faeee075b3fa1038a6a8bb
Fabian Koch <fabian3.koch@tu-dortmund.de>
2018-06-17 21:57:32 +0200

    Gamma done

--- V704/rechner.py
+++ V704/rechner.py
@@ -17,6 +17,17 @@ akt=cts-null
 akterr = (ctserr-np.sqrt(950))/t
 print('akt: ', akt, '+-', akterr )
 
+r=2.82e-15
+epsilon= 2.45
+z = 30
+Molvol = 9.16e-6
+sigma = 2 * np.pi * r**2 * ((1+epsilon) / (epsilon**2) * ((2 * (1+epsilon)) / (1+2 * epsilon) - 1/(epsilon) * np.log(1+epsilon)) + 1/(2 * epsilon) * np.log(1 + 2 * epsilon) - (1+3 * epsilon)/ ((1+2 * epsilon)**2))
+n = (z * 6.02214085774e23)/(Molvol)
+mu = sigma * n
+
+print('sigma: ', sigma)
+print('n: ', n)
+print('mu: ', mu)
 #Für Blei:
 
 print('----------------------------------------------')
@@ -30,3 +41,12 @@ nullb=950/900
 aktb=ctsb-nullb
 aktberr = (ctsberr-np.sqrt(950))/tb
 print('aktb: ', aktb, '+-', aktberr )
+
+zb = 82
+Molvolb = 18.26e-6
+nb = (zb * 6.02214085774e23)/(Molvolb)
+mub = sigma * nb
+
+print('sigma: ', sigma)
+print('nb: ', nb)
+print('mub: ', mub)
