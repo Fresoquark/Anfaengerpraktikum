60346e10d625f24676a2b1cdf98cf584380baf1a
Tom Rosenstein <tom.rosenstein@tu-dortmund.de>
2018-07-02 10:15:17 +0200

    Auswertung angepasst.

--- US2/rechner.py
+++ US2/rechner.py
@@ -20,9 +20,14 @@ unten = unten-0.43
 komplett = 60.55-0.43
 c = 2730*0.001 #mm/μs
 
+
 obenm = 1/2*c*oben
 untenm = 1/2*c*unten
 komplettm = 1/2*c*komplett
+schutzschicht=komplettm-80.45
+komplettm=komplettm-schutzschicht
+obenm=obenm-schutzschicht
+untenm= untenm-schutzschicht
 print('komplettm= ', komplettm)
 dicke = komplettm-(obenm+untenm)
 np.savetxt("data/ascantab.csv",np.column_stack([nummer,oben,obenm,unten,untenm,dicke]),delimiter=",",fmt=["%3.0f","%3.2f","%3.2f","%3.2f","%3.2f","%3.2f"])
@@ -33,6 +38,8 @@ oben = np.array([15.12-0.43, 13.74-0.43])
 unten = np.array([44.34-0.43, 45.60-0.43])
 obenm = (1/2)*c*oben
 untenm = (1/2)*c*unten
+obenm=obenm-schutzschicht
+untenm= untenm-schutzschicht
 dicke = komplettm-(obenm+untenm)
 nummer2 = np.delete(nummer,[2,3,4,5,6,7,8,9,10,11])
 #frequenz = np.array([1,2])
@@ -43,11 +50,10 @@ np.savetxt("data/auflosungtab.csv",np.column_stack([nummer2,oben,obenm,unten,unt
 oben, unten = np.genfromtxt("data/bscan.csv",delimiter=",",unpack=True)
 oben = np.delete(oben,[9])
 unten = np.delete(unten,[9])
-oben = oben-2.58
-unten = unten-2.58
+#oben = oben-2.58
+#unten = unten-2.58
 obenm = (1/2)*c*oben
 untenm = (1/2)*c*unten
-komplettm = 76.75
 dicke = komplettm-(obenm+untenm)
 nummer3 = np.delete(nummer,[9])
 np.savetxt("data/bscantab.csv",np.column_stack([nummer3,oben,obenm,unten,untenm,dicke]),delimiter=",",fmt=["%3.0f","%3.2f","%3.2f","%3.2f","%3.2f","%3.2f"])
