17f8251df680391845d7e3d7a11a98e1da037ca8
Tom Rosenstein <tom.rosenstein@tu-dortmund.de>
2018-07-05 13:20:19 +0200

    US3 erstellt. Daten aufgenommen. Python Datei für mittleres Rohr erstellt. Tabelle funktioniert irgendwie nicht, und ich weiß echt nicht wie viele Plots wir erstellen sollen.

---
+++ US3/data/readme.txt
@@ -0,0 +1,8 @@
+Prismenwinkel / Dopplerwinkel:
+15 / 80.06
+30 / 70.53
+60 / 54.74
+
+Bei den .csv Dateien zu dick, dünn und mittel sind in der ersten Spalte die RPM, dann 15°, dann 30 dann 60.
+Bei vprofil ist die erste Spalte die µs, die zweite und dritte die 70% Messung und die vierte und fünfte erklärt sich dann wohl von selbst.
+Alle Frequenzangaben sind in Hz.  
---
+++ US3/mittel.py
@@ -0,0 +1,40 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.stats import sem
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import scipy.constants as const
+
+rpm,a,b,c=np.genfromtxt("data/mittel.csv",delimiter=",",unpack=True)    # a=15 b=30 c=60
+vfluss=(100/9200)*rpm   #Fließgeschwindigkeit in Prozent
+alpha=np.array([80.06,70.53,54.74])
+alpha=np.deg2rad(alpha)
+nu0=2000000 #nu0 in Hz
+cl=1800 #Schallgeschwindigkeit in m/s
+v15=(a*cl)/(2*nu0*np.cos(alpha[0]))
+v30=(b*cl)/(2*nu0*np.cos(alpha[1]))
+v60=(c*cl)/(2*nu0*np.cos(alpha[2]))
+
+vm=np.array([np.mean([v15[0],v30[0],v60[0]]),np.mean([v15[1],v30[1],v60[1]]),np.mean([v15[2],v30[2],v60[2]]),np.mean([v15[3],v30[3],v60[3]]),np.mean([v15[4],v30[4],v60[4]])])
+vmerr=np.array([sem([v15[0],v30[0],v60[0]]),sem([v15[1],v30[1],v60[1]]),sem([v15[2],v30[2],v60[2]]),sem([v15[3],v30[3],v60[3]]),sem([v15[4],v30[4],v60[4]])])
+
+
+
+np.savetxt("data/mitteltab.csv",np.column_stack([vfluss,a,v15,b,v30,c,v60,vm,vmerr]),delimiter=",",fmt=["%2.0f","%4.0f","%4.2f","%4.0f","%4.2f","%4.0f","%4.2f","%4.2f","%4.2f"])
+
+
+
+
+
+yplot=a/np.cos(alpha[0])
+
+plt.plot(v30, yplot, 'k.', label="Daten", ms=2.5)
+
+plt.xlabel(r'v / $\si{\metre\per\second}$')
+plt.ylabel(r'$\frac{\Delta\nu}{\cos(\alpha)}$ / $\si{\hertz}$')
+plt.legend(loc='best')
+plt.grid()
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/mittel30.pdf')
